3cc57933f0814061b1e4e71d5c7d3e230a6911dd
Ela Kozera <kozerka@gmail.com>
2024-11-21 12:57:41 +0100

    chore: config sanity studio access

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import sanity from '@sanity/astro';
-import react from '@astrojs/react';
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sanity(), react()]
+    integrations: [
+        sanity({
+            projectId: "di98mlum",
+            dataset: "production",
+            useCdn: false,
+            apiVersion: "2024-07-24",
+            studioBasePath: "/studio",
+        }),
+        ,
+        react(),
+    ],
 });
