b3583133ba38b39430eba3df00c7ddae82033ba6
Ela Kozera <kozerka@gmail.com>
2024-11-26 19:32:47 +0100

    feat: create main project files structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,18 +5,26 @@ import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        sanity({
-            projectId: "di98mlum",
-            dataset: "production",
-            useCdn: false,
-            apiVersion: "2024-07-24",
-            studioBasePath: "/studio",
-        }),
-        ,
-        react(),
-        tailwind(),
-    ],
+  integrations: [
+      sanity({
+          projectId: "di98mlum",
+          dataset: "production",
+          useCdn: false,
+          apiVersion: "2024-07-24",
+          studioBasePath: "/studio",
+      }),
+      ,
+      react(),
+      tailwind(),
+  ],
+
+  output: "server",
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
